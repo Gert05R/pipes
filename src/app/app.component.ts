9e081d48059ba4d1469f1a97f338dfb98772b267
GERT05 <raeymaekers.gert1@gmail.com>
2022-08-18 00:37:43 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name= '';
+  date= '';
+  amount= 0;
+  height= 0;
+  miles= 0;
+
+  car = {
+
+    make: 'Mercedes',
+    model: 'C-class',
+    year: '2023'
+
+  };
+
+  onMilesChange(value: string) 
+  {
+    this.miles = parseFloat(value);
+  }
+
+  onHeightChange(value: string) 
+  {
+    this.height = parseFloat(value);
+  }
+
+  onNameChange(value: string) {
+
+    this.name = value;
+  } 
+
+  onDateChange(value: string) 
+  {
+    this.date=value;
+  }
+
+  onAmountChange(value: string) 
+  {
+    this.amount = parseFloat(value);
+  }
+  
 }
